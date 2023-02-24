64b76e03387215ba0a85bf18d85cb7405be1b056
geekpf <50725274+geekpf@users.noreply.github.com>
2023-02-23 21:17:50 -0300

    Add files via upload

---
+++ codigo1.rb
@@ -0,0 +1,15 @@
+# pedir nome, sobrenome e idade do usuario
+puts "Digite seu nome"
+nome = gets.chomp
+
+puts "Digite seu sobrenome"
+
+sobrenome = gets.chomp
+
+puts "Digite sua idade"
+
+idade = gets.chomp
+
+#exibir os dados
+
+puts  "Dados do usuário. Nome : #{nome} #{sobrenome} possui #{idade} anos."
